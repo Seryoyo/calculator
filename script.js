96ceb6fa3246799bfd8a6f1c2fe959664546ecb9
Sergio <harogio101@gmail.com>
2023-08-19 18:37:25 -0700

    create basic calculator functions, add, subtract, multiply, divide, power

---
+++ script.js
@@ -0,0 +1,21 @@
+function add(a, b){
+    return a + b;
+}
+
+function subract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+function power(a, b){
+    return Math.pow(a, b);
+}
+
+console.log(add(5,7));
